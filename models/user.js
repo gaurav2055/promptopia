7f930e8e3552a3fbda0e1717a9c7fd789bfb1ba0
Gaurav Suvarna <gauravs712118@gmail.com>
2023-07-13 21:16:01 +0530

    no destructuring mongose

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
-import { Schema, model, models } from "mongoose";
+// import { Schema, model, models } from "mongoose";
+import mongoose from "mongoose";
 
-const UserSchema = new Schema({
+const UserSchema = new mongoose.Schema({
     email: {
         type: String,
         unique: [true, "Email already exist"],
@@ -19,6 +20,6 @@ const UserSchema = new Schema({
     },
 });
 
-const User = models?.User || model("User", UserSchema);
+const User = mongoose.models?.User || mongoose.model("User", UserSchema);
 
 export default User;
